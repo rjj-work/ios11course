52071348a3cd637415854b80a70346f4ed8b695e
rjj-work <rjj.work@gmail.com>
2018-11-01 22:42:02 -0400

    Second app, like the first, but from scratch.

--- SecondApp/SecondApp/SecondApp/ViewController.swift
+++ SecondApp/SecondApp/SecondApp/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageField: UITextField!
+
+    @IBOutlet weak var outputLabel: UILabel!
+
+    @IBAction func clickMeSeymore(_ sender: Any) {
+        print("Seymore clicked")
+
+        if let age = ageField.text {
+            outputLabel.text = "You are " + age + " years old."
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
