6d774359d5651a65c4863a465a89654a865b3301
rjj-work <rjj.work@gmail.com>
2018-12-20 08:56:56 -0500

    Added outlets for input of human age and display of cat age. Button to initiate calculation; checks to ensure there is some data there. Constant(s) in their own struct and declared.
    Known issues: using Float, when I'd prefer to use Integer values.
    I should probably make the display a non-editable field of some kind, probably a Label.

---
+++ CatYears/CatYears/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CatYears
+//
+//  Created by rjj on 2018.12.19.
+//  Copyright © 2018 Sapient, Inc. All rights reserved.
+//
+// Reference: https://stackoverflow.com/questions/26252233/global-constants-file-in-swift
+
+import Foundation
+
+struct Factors {
+
+    static let HumanYearsToCatYears = Float(7)
+
+}
--- CatYears/CatYears/ViewController.swift
+++ CatYears/CatYears/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageInput: UITextField!
+
+    @IBOutlet weak var ageOutput: UITextField!
+
+    @IBAction func getAgeInCatYears(_ sender: UIButton) {
+        // extract data from input
+        if let ageStr = ageInput.text {
+
+            // check if the data is numeric
+            let age = Float(ageStr)
+            if age == nil {
+                print("value '" + ageStr + "' not a number")
+                return
+            }
+            print("numeric string value: '" + ageStr + "'")
+
+            // multiple by human to cat years factor
+            let catAge = age! * Factors.HumanYearsToCatYears
+
+            let catAgeStr = String(format: "%0.0f", catAge)
+            print("catAgeStr" + catAgeStr)
+
+            // update output
+            //print(String(format: "humageAge: %s, factor: %d, catAge: %d, catAgeStr: %s", ageStr, Factors.HumanYearsToCatYears, catAge, catAgeStr))
+
+            ageOutput.text = catAgeStr
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
