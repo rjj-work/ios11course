29406f7f48619f92f49a58c814c2a04daae39ef8
rjj-work <rjj.work@gmail.com>
2019-01-26 22:27:01 -0500

    Project IsItPrime initial generated coded

---
+++ S04/IsItPrime/IsItPrime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IsItPrime
+//
+//  Created by rjj on 2019.01.26.
+//  Copyright © 2019 Sapient, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
