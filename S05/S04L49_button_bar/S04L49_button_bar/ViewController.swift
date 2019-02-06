69331b043bcf03bdfac672b1040420183d83a3a8
rjj-work <rjj.work@gmail.com>
2019-02-05 22:36:39 -0500

    S04L49 leads to S05 EggTimer example

---
+++ S05/S04L49_button_bar/S04L49_button_bar/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  S04L49_button_bar
+//
+//  Created by rjj on 2019.01.31.
+//  Copyright © 2019 Sapient, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func bbCamera(_ sender: Any) {
+        print("bbCamera pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
