7baaa8514abe6c5cd677941a5a754bc231d9d7f8
rjj-work <rjj.work@gmail.com>
2019-02-05 22:56:54 -0500

    Now compiles and runs

--- S05/S04L49_button_bar/S04L49_button_bar/ViewController.swift
+++ S05/S04L49_button_bar/S04L49_button_bar/ViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    var timer = Timer()
+
     @IBAction func bbCamera(_ sender: Any) {
-        print("bbCamera pressed")
+        print("bbCamera pressed - invalidating \(timer)")
+        timer.invalidate()
+    }
+
+    @objc func processTimer() {
+        print("Timer fired: once per second")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
 
 }
 
