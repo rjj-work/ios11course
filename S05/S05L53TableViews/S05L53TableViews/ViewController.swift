3a091305fb5152cd14d436b29bab33a23895067a
rjj-work <rjj.work@gmail.com>
2019-02-23 22:55:34 -0500

    Initial generation from Xcode

---
+++ S05/S05L53TableViews/S05L53TableViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  S05L53TableViews
+//
+//  Created by rjj on 2019.02.23.
+//  Copyright © 2019 Sapient, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
