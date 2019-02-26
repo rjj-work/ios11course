952233a65b2134fd442719de4d958999c975f1a6
rjj-work <rjj.work@gmail.com>
2019-02-25 20:57:19 -0500

    Table with number of rows determined by array

--- S05/S05L53TableViews/S05L53TableViews/ViewController.swift
+++ S05/S05L53TableViews/S05L53TableViews/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let cellContent = ["Timber", "Teddy", "Belle", "Cali", "Merida"]
+
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+
+        cell.textLabel?.text = cellContent[indexPath.row]
+
+        return cell
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
